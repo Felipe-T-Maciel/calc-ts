fece360723be4489bda3b4b7b7a79f92aaf2eeb0
FELIPE TOMIO MACIEL <felipe_t_maciel@estudante.sesisenai.org.br>
2023-04-24 21:48:34 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { calcModule } from './calc/calc.Module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    calcModule
   ],
   providers: [],
   bootstrap: [AppComponent]
